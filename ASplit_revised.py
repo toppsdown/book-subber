44cc26e9fab230d426022a0030f5cacc86eee746
Toppsdown <puzarne.e@gmail.com>
2019-11-30 20:44:41 -0500

    rename variables for easier reading

--- ASplit_revised.py
+++ ASplit_revised.py
@@ -4,36 +4,38 @@ import numpy
 
 FFMPEG_BIN = "ffmpeg"
 
-print 'ASplit.py <src.m4a> <silence duration in seconds> <threshold amplitude 0.0 .. 1.0>'
+print 'ASplit.py <src.mp3> <silence duration in seconds> <thresheshold amplitude 0.0 .. 1.0>'
 
 src = sys.argv[1]
-dur = float(sys.argv[2])
-thr = int(float(sys.argv[3]) * 65535)
+duration_s = float(sys.argv[2])
+
+max_amplitude = 65535
+threshold = int(float(sys.argv[3]) * max_amplitude)
 
 f = open('%s-out_revised.txt' % src, 'wb')
 
-tmprate = 22050
-len2 = dur * tmprate
-buflen = int(len2     * 2)
+sample_rate = 22050
+num_samples_in_set = duration_s * sample_rate
+buflen = int(num_samples_in_set     * 2)
 #            t * rate * 16 bits
 
-oarr = numpy.arange(1, dtype='int16')
+last_sample_set = numpy.arange(1, dtype='int16')
 # just a dummy array for the first chunk
 
 command = [ FFMPEG_BIN,
         '-i', src,
         '-f', 's16le',
         '-acodec', 'pcm_s16le',
-        '-ar', str(tmprate), # ouput sampling rate
+        '-ar', str(sample_rate), # ouput sampling rate
         '-ac', '1', # '1' for mono
         '-']        # - output to stdout
 
 pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
 
 tf = True
-pos = 0
-opos = 0
-part = 0
+current_processing_timestamp = 0
+sample_start_timestamp = 0
+audio_segment_count = 0
 
 while tf :
 
@@ -42,27 +44,27 @@ while tf :
         tf = False
         break
 
-    arr = numpy.fromstring(raw, dtype = "int16")
+    current_sample_set = numpy.fromstring(raw, dtype = "int16")
 
-    rng = numpy.concatenate([oarr, arr])
-    mx = numpy.amax(rng)
-    if mx <= thr :
+    rng = numpy.concatenate([last_sample_set, current_sample_set])
+    max_amplitude = numpy.amax(rng)
+    if max_amplitude <= threshold :
         # the peak in this range is less than the threshold value
-        trng = (rng <= thr) * 1
+        trng = (rng <= threshold) * 1
         # effectively a pass filter with all samples <= thr set to 0 and > thr set to 1
-        sm = numpy.sum(trng)
+        num_samples_below_threshold = numpy.sum(trng)
         # i.e. simply (naively) check how many 1's there were
-        if sm >= len2 :
-            part += 1
-            apos = pos + dur * 0.5
-            print mx, sm, len2, apos
-            f.write('ffmpeg -i "%s" -ss %f -to %f -c copy -y "%s-p%04d.m4a"\r\n' % (src, opos, apos, src, part))
-            opos = apos
+        if num_samples_below_threshold >= num_samples_in_set :
+            audio_segment_count += 1
+            sample_end_timestamp = current_processing_timestamp + duration_s * 0.5
+            print max_amplitude, num_samples_below_threshold, num_samples_in_set, sample_end_timestamp
+            f.write('ffmpeg -i "%s" -ss %f -to %f -c copy -y "%s-p%04d.m4a"\r\n' % (src, sample_start_timestamp, sample_end_timestamp, src, audio_segment_count))
+            sample_start_timestamp = sample_end_timestamp
 
-    pos += dur
+    current_processing_timestamp += duration_s
 
-    oarr = arr
+    last_sample_set = current_sample_set
 
-part += 1    
-f.write('ffmpeg -i "%s" -ss %f -to %f -c copy -y "%s-p%04d.m4a"\r\n' % (src, opos, pos, src, part))
+audio_segment_count += 1
+f.write('ffmpeg -i "%s" -ss %f -to %f -c copy -y "%s-p%04d.m4a"\r\n' % (src, sample_start_timestamp, current_processing_timestamp, src, audio_segment_count))
 f.close()
