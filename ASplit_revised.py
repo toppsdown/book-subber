24fbaf5ef84dca026ab474e4af1704034d090233
Toppsdown <puzarne.e@gmail.com>
2019-11-30 20:50:28 -0500

    add comments to explain the script

--- ASplit_revised.py
+++ ASplit_revised.py
@@ -16,11 +16,19 @@ f = open('%s-out_revised.txt' % src, 'wb')
 
 sample_rate = 22050
 num_samples_in_set = duration_s * sample_rate
+
+# create a buffer length that can take two samples of given duration
 buflen = int(num_samples_in_set     * 2)
 #            t * rate * 16 bits
 
 last_sample_set = numpy.arange(1, dtype='int16')
 # just a dummy array for the first chunk
+
+
+# s16le audiotype:
+# PCM signed 16-bit little-endian
+# Represents the waveform as a series of values from 0-65536
+# if sample is signed, sample range -32768-32767 midpoint is 0
 
 command = [ FFMPEG_BIN,
         '-i', src,
@@ -39,15 +47,36 @@ audio_segment_count = 0
 
 while tf :
 
+    # grab 2x sample set duration from the pipe
+    # Eg. if duration is 1 sec, take two seconds of audio
+
     raw = pipe.stdout.read(buflen)
+
+    # If end of file, close program
     if raw == '' :
         tf = False
         break
 
+    # when 2 bytes are used for audio samples, need to order bytes
+    # Example: 0x9A8B => 1001 1010 1000 1011
+    # little-endian: 8B, 9A => [1000 1011],[1001 1010]
+    # big-endian: 9A, 8B => [1001 1010], [1000 1011]
+    # Does this correctly detect little endian?
+    # Looks like it understand little endian correctly and it's signed
+    # numpy.fromstring("\x02\x01", dtype = "int16") # => 258
+    # numpy.fromstring("\x02\x02", dtype = "int16") # => 514
+    # numpy.fromstring("\x00\x80", dtype = "int16") # => -32768
+    # numpy.fromstring("\x00\x80\x00\x80", dtype = "int16") # => [-32768,-32768]
     current_sample_set = numpy.fromstring(raw, dtype = "int16")
 
     rng = numpy.concatenate([last_sample_set, current_sample_set])
+
+    # Because this is a waveform, each sample is just volume
+    # Get the maximum absolute value within the range
     max_amplitude = numpy.amax(rng)
+
+    # Threshold is calculated based on percentage of range
+    # the peak in this range is less than the thresheshold value
     if max_amplitude <= threshold :
         # the peak in this range is less than the threshold value
         trng = (rng <= threshold) * 1
